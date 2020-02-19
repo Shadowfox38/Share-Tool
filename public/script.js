3149800a6931a32499eeae79ceeb11c9c4639ede
shadowfox38 <akhil3800@gmail.com>
2020-02-19 13:11:10 +0530

    fixed bug

--- public/script.js
+++ public/script.js
@@ -10,19 +10,25 @@ function getdata(path)
     if(xhr.readyState == 4 && xhr.status == 200)
     {
       var myobj = JSON.parse(this.responseText);
+      var path;
       dirs = myobj.dirs;
       files = myobj.files;
       curr_dir = myobj.curr_dir;
       if(curr_dir == "/"){curr_dir = "";}
       $(".list1").empty();
+      console.log("encodeURIComponent('test?download') = " + encodeURIComponent('test?download'));
       dirs.forEach(function(item)
       {
-        $(".list1").append('<li><a href = "' + curr_dir + '/'  + item  + '">' + item + '</a></li>');
+        path = curr_dir + "/" + item;
+        path = encodeURIComponent(path);
+        $(".list1").append('<li><a href = "' + path + '">' + item + '</a></li>');
       });
       $(".list2").empty();
       files.forEach(function(item)
       {
-        $(".list2").append('<li><a href = "' + curr_dir + '/'  + item + '?download' + '">' + item + '</a></li>');
+        path = curr_dir + "/" + item;
+        path = encodeURIComponent(path);
+        $(".list2").append('<li><a href = "' + path + '?download' + '">' + item + '</a></li>');
       });
     }
   }
@@ -30,5 +36,7 @@ function getdata(path)
 $(document).ready(function()
 {
   var x = $(location).attr('pathname');
+  if(x != '/')
+    x = x + '/';
   getdata(x);
 });
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 var app = express();
 var root = "/home/shadowfox",curr_dir = "",whole_path;
 app.use(express.static('public'));
 app.use('/static',express.static('public'));
+app.use(express.urlencoded({extended :true}));
 app.get('*',function(req,res)
 {
   var q = url.parse(req.url,true);
-  var path = q.pathname;
-  console.log(path);
+  var filePath = q.pathname;
+  filePath = unescape(filePath);
+  filePath = filePath.split(" ").join("\ ");
   q = q.search;
   if(q == '?download')
-    res.download(root + "/" + path);
+    res.download(root + "/" + filePath);
   else
     res.sendFile(__dirname + '/src/index.html')
 });
@@ -20,7 +23,11 @@ app.put('*',function(req,res)
 {
   var dirs = [],files = [],temp;
   curr_dir = req.url;
-  whole_path = root + curr_dir
+  curr_dir = unescape(curr_dir);
+  var xx = curr_dir.split(" ");
+  curr_dir = xx.join("\ ");
+  whole_path = path.join(root,curr_dir);
+  console.log("reading dir : " + whole_path);
   fs.readdir(whole_path,function(err,data)
   {
     data = data.filter(item => !(/(^|\/)\.[^\/\.]/g).test(item));
